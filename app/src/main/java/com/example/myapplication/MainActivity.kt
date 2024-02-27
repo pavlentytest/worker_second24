8d6dc6399180f5d3b54aab5e4b1dd2df3f9fe909
Pavel Stepanov <impetus@yandex.ru>
2024-02-27 13:42:30 +0300

    additions

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,22 +1,40 @@
 package com.example.myapplication
 
+import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import androidx.lifecycle.Observer
+import androidx.work.Constraints
 import androidx.work.Data
 import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.PeriodicWorkRequestBuilder
+import androidx.work.WorkInfo
 import androidx.work.WorkManager
 import androidx.work.WorkRequest
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+
         val data: Data = Data.Builder().putString("key1","hello").build()
+
+        val constraint = Constraints.Builder().setRequiresCharging(true).build()
+
+        val worker5 = PeriodicWorkRequestBuilder<MyWorker2>(1, TimeUnit.MINUTES).build() // 15 min - min
+
+        val worker6 = PeriodicWorkRequestBuilder<MyWorker2>(1, TimeUnit.HOURS, 15, TimeUnit.MINUTES);
+
 
         val worker = OneTimeWorkRequestBuilder<MyWorker>()
             .setInputData(data)
+            .addTag("worker2")
+            .setConstraints(constraint)
             .build()
 
         val worker2 = OneTimeWorkRequestBuilder<MyWorker2>().build()
@@ -40,5 +58,13 @@ class MainActivity : AppCompatActivity() {
 
             */
         }
+
+        WorkManager.getInstance(this).getWorkInfoByIdLiveData(worker.id)
+            .observe(this, Observer { workerData: WorkInfo ->
+                if(workerData != null) {
+                    Log.d("RRR",workerData.state.toString())
+                    workerData.outputData.getString("key2")?.let { Log.d("RRR", it) }
+                }
+            })
     }
 }
--- app/src/main/java/com/example/myapplication/MyWorker.kt
+++ app/src/main/java/com/example/myapplication/MyWorker.kt
@@ -2,14 +2,17 @@ package com.example.myapplication
 
 import android.content.Context
 import android.util.Log
+import androidx.work.Data
 import androidx.work.Worker
 import androidx.work.WorkerParameters
 
 class MyWorker(context: Context, params: WorkerParameters) : Worker(context, params) {
     override fun doWork(): Result {
+        val str = inputData.getString("key1")
         Log.d("RRR","MyWorker1 started")
         Thread.sleep(3000)
         Log.d("RRR","MyWorker1 completed!")
-        return Result.success()
+        val data: Data = Data.Builder().putString("key2","hello").build()
+        return Result.success(data)
     }
 }
